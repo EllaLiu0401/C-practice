7ff4206100306877cdf354cda267d038aab809c5
EllaLiu0401 <ellatong0515@gmail.com>
2024-10-05 13:50:43 +1000

    WriteLine

--- DotNet-starter/HelloWorld/Program.cs
+++ DotNet-starter/HelloWorld/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, C# Programmers!");
+Console.Write("First");
+Console.Write("Second");
+
+// Hello, C# Programmers!
+// FirstSecond% 
